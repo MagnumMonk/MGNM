2f9bfc914bd0442cba60db7b4fdfc4adc6886839
MagnumMonk <ioliverblack@gmail.com>
2017-06-01 14:25:29 +0300

    parallax

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
+
+
 $(document).ready(function(){
     $("li").hide();
     $('.drop-panel').hover(function() {
         $('li').slideToggle(300);
     });
   });
+
   $('.bubble').hover(
      function(){
       $(this).toggleClass('hover');
   });
 
   $('.block-image').hover(
-     function(){
+     function() {
       $(this).toggleClass('hoverimg');
+  });
+
+  /*  var images = ["images/bg/pexels-photo-27403.jpg",
+                  "images/bg/botbg.jpg"];
+
+    function getRandomInt()
+    {
+      return Math.floor(Math.random() * images.length);
+    };
+
+    function nextBackground() {
+      $(".banner").css("background-image", "url(" + images[getRandomInt()] +")");
+    };
+  setInterval(nextBackground, 4500); */
+
+  $(document).ready(function(){
+      $('body').mousemove(function(e){
+         var x = -(e.pageX + this.offsetLeft) / 20;
+          var y = -(e.pageY + this.offsetTop) / 20;
+          $('.banner').css('background-position', x + 'px ' + y + 'px' );
+          $('#prlx').css('background-position', x/2 + 'px ' + y/2 + 'px' );
+      });
   });
